0d693a35c1a10e0d94f5e069f70f3e287a7df1c7
Clayton Pereira <clayton.pereira@outlook.com>
2022-04-22 16:51:26 -0300

    BackEnd Cap-2 - Created Project TDD UnSolved Challenge

---
+++ src/main/java/com/devsuperior/tddresolvido/TDDResolvidoApplication.java
@@ -0,0 +1,12 @@
+package com.devsuperior.tddresolvido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TDDResolvidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TDDResolvidoApplication.class, args);
+	}
+}
