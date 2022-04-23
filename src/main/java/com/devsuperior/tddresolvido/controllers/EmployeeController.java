08b2c2921a9b6a11d99bbe99d7b01a8a2042b167
Clayton Pereira <clayton.pereira@outlook.com>
2022-04-23 14:33:43 -0300

    BackEnd Cap-2 - Created EmployeeController newEmployee - Test OK

--- src/main/java/com/devsuperior/tddresolvido/controllers/EmployeeController.java
+++ src/main/java/com/devsuperior/tddresolvido/controllers/EmployeeController.java
@@ -1,13 +1,18 @@
 package com.devsuperior.tddresolvido.controllers;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.tddresolvido.dto.EmployeeDTO;
 import com.devsuperior.tddresolvido.services.EmployeeService;
@@ -20,9 +25,17 @@ public class EmployeeController {
 	private EmployeeService employeeService;
 	
 	@GetMapping
-	public ResponseEntity<Page<EmployeeDTO>> findAll(@PageableDefault(sort = "name") Pageable pageable) {
+	public ResponseEntity<Page<EmployeeDTO>> findAll(@PageableDefault(sort = "name") Pageable pageable) {		
 		Page<EmployeeDTO> list = employeeService.findAllPaged(pageable);
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@PostMapping
+	public ResponseEntity<EmployeeDTO> insert(@RequestBody EmployeeDTO employeeDto) {
+		employeeDto = employeeService.insert(employeeDto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+				.buildAndExpand(employeeDto.getId()).toUri();
+		return ResponseEntity.created(uri).body(employeeDto);
 	}
 
 }
--- src/main/java/com/devsuperior/tddresolvido/services/EmployeeService.java
+++ src/main/java/com/devsuperior/tddresolvido/services/EmployeeService.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.tddresolvido.dto.EmployeeDTO;
+import com.devsuperior.tddresolvido.entities.Department;
 import com.devsuperior.tddresolvido.entities.Employee;
 import com.devsuperior.tddresolvido.repositories.EmployeeRepository;
 
@@ -15,9 +17,22 @@ public class EmployeeService {
 	@Autowired
 	private EmployeeRepository employeeRepository;
 	
+	@Transactional(readOnly = true)
 	public Page<EmployeeDTO> findAllPaged(Pageable pageable) {
 		Page<Employee> employees = (Page<Employee>) employeeRepository.findAll(pageable);
 		return employees.map(x -> new EmployeeDTO(x));
+	}
+	
+	@Transactional
+	public EmployeeDTO insert(EmployeeDTO employeeDto) {
+		Employee employee = new Employee();
+		employee.setName(employeeDto.getName());
+		employee.setEmail(employeeDto.getEmail());
+		employee.setDepartment(new Department(employeeDto.getDepartmentId(), null));
+		
+		employeeRepository.save(employee);
+		
+		return new EmployeeDTO(employee);
 	}
 
 }
